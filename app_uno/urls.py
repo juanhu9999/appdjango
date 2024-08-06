b61fcb0469580dc2db16aaa7ee156a636c607ad7
juanhu9999 <juanpablo.isabella@gmail.com>
2024-08-06 09:33:24 -0300

    update

--- app_uno/urls.py
+++ app_uno/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from app_uno.views import inicio, cursos, estudiantes
 
 urlpatterns = [
-    path('', inicio),
-    path('cursos', cursos),
-    path('estudiantes', estudiantes),
+    path('', inicio, name="inicio"),
+    path('cursos', cursos,name="cursos"),
+    path('estudiantes', estudiantes, name="estudiantes"),
 
 ]
